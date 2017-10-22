693cf16688b301cde4b3fd56ac8379a08062743b
David van Elk <david.vanelk96@gmail.com>
2017-10-23 00:17:47 +0200

    Added EF Support for MySQL

--- box.security/Config.cs
+++ box.security/Config.cs
@@ -42,5 +42,14 @@ namespace box.security
                 }
             };
         }
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        }
+
     }
 }
--- box.security/Startup.cs
+++ box.security/Startup.cs
@@ -8,6 +8,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using IdentityServer4.EntityFramework.Mappers;
+using IdentityServer4.EntityFramework.DbContexts;
 
 namespace box.security
 {
@@ -32,9 +36,23 @@ namespace box.security
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
             //Configure IdentityServer 4
             services.AddIdentityServer()
                 .AddDeveloperSigningCredential()
+                //Use MySQL-Server for Configuration-store
+                .AddConfigurationStore(options =>
+                {
+                    options.ConfigureDbContext = builder =>
+                        builder.UseMySql(Configuration.GetConnectionString("MySql"), sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
+                //Use MySQL-Server for Operational-Store
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = builder =>
+                        builder.UseMySql(Configuration.GetConnectionString("MySql"), sql => sql.MigrationsAssembly(migrationsAssembly));
+                })
                 .AddInMemoryApiResources(Config.GetApiResources())
                 .AddInMemoryClients(Config.GetClients());
 
@@ -50,9 +68,55 @@ namespace box.security
                 app.UseDeveloperExceptionPage();
             }
 
+            //Initialize Database for IdentityServer 4
+            InitializeDatabase(app);
+
             //Start IdentityServer 4
             app.UseIdentityServer();
             app.UseMvc();
+        }
+
+        /// <summary>
+        /// Initializes the Database for ID-Server
+        /// </summary>
+        /// <param name="app"></param>
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.EnsureCreated();
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.EnsureCreated();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in Config.GetClients())
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in Config.GetIdentityResources())
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in Config.GetApiResources())
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
